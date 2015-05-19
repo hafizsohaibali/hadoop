b206660e51b3636edf41a22338145febda946c8d
hafizsohaibali <hafizsohaibali@gmail.com>
2015-05-19 21:46:31 +0500

    sample

--- command.sh
+++ command.sh
@@ -1,2 +1,12 @@
-bin/hadoop jar contrib/streaming/hadoop-*streaming*.jar -file /mnt/code/hadoop/mapper.py -mapper /mnt/code/hadoop/mapper.py -file /mnt/code/hadoop/reducer.py -reducer /mnt/code/hadoop/reducer.py -input /user/input/train/01IsoiSMh5gxyDYTl4CB.bytes  -output /user/input/output
-
+hadoop fs -rm -r  /user/input/train/bytes
+echo '1000 start' >> /mnt/code/output/time.txt
+date +%s >> /mnt/code/output/time.txt
+bin/hadoop jar 
+contrib/streaming/hadoop-*streaming*.jar 
+-file /mnt/code/hadoop/mapper.py -mapper /mnt/code/hadoop/mapper.py 
+-file /mnt/code/hadoop/reducer.py -reducer /mnt/code/hadoop/reducer.py 
+-input /user/input/train/bytes/01kcPWA9K2BOxQeS5Rju.bytes
+-input /user/input/train/bytes/04EjIdbPV5e1XroFOpiN.bytes
+-output /user/input/train/output
+date +%s >> /mnt/code/output/time.txt
+echo '1000 finish' >> /mnt/code/output/time.txt
