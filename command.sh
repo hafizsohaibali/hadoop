79cfeb4f154801f40d1d5910ede1677c7bfc07a5
hafizsohaibali <hafizsohaibali@gmail.com>
2015-05-21 03:34:07 +0500

    shell output

--- command.sh
+++ command.sh
@@ -1,6 +1,6 @@
 hadoop fs -rm -r  /user/input/output
 echo '5GB year  start' >> /mnt/code/output/time.txt
 date +%s >> /mnt/code/output/time.txt
-bin/hadoop jar contrib/streaming/hadoop-*streaming*.jar -file /mnt/code/hadoop/mapperflight.py -mapper /mnt/code/hadoop/mapperflight.py -file /mnt/code/hadoop/reducerflight.py -reducer /mnt/code/hadoop/reducerflight.py -input /user/input/airport/airport-out-put.csv -output /user/input/output
+bin/hadoop jar contrib/streaming/hadoop-*streaming*.jar -file /mnt/code/hadoop/mapperflight.py -mapper /mnt/code/hadoop/mapperflight.py -file /mnt/code/hadoop/reducerflight.py -reducer /mnt/code/hadoop/reducerflight.py -input /user/input/airport/airport-output.csv -output /user/input/output
 date +%s >> /mnt/code/output/time.txt
 echo '5GB year finish' >> /mnt/code/output/time.txt
