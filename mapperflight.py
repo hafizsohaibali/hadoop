15dc18215bb40e843d555e7630e66c086c591d97
hafizsohaibali <hafizsohaibali@gmail.com>
2015-05-21 02:28:08 +0500

    dd

--- mapperflight.py
+++ mapperflight.py
@@ -7,9 +7,7 @@ index =0
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
-	if index==0:
-		continue;
-    # split the line into words
+	# split the line into words
     data = line.split(',')
 	print line 
     year = data[0]
