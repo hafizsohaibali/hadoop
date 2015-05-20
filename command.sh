771141b0d0ddbd5914b9d4a7d42c81d0cffb2f7a
hafizsohaibali <hafizsohaibali@gmail.com>
2015-05-21 02:10:26 +0500

    max distance

--- command.sh
+++ command.sh
@@ -1,4 +1,4 @@
-hadoop fs -rm -r  /user/input/train/output
+hadoop fs -rm -r  /user/input/output
 echo '5GB MAX distance start' >> /mnt/code/output/time.txt
 date +%s >> /mnt/code/output/time.txt
 bin/hadoop jar contrib/streaming/hadoop-*streaming*.jar -file /mnt/code/hadoop/mapperflight.py -mapper /mnt/code/hadoop/mapperflight.py -file /mnt/code/hadoop/reducerflight.py -reducer /mnt/code/hadoop/reducerflight.py -input /user/input/airport/airport-out-put.csv -output /user/input/output
