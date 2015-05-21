8d6b063b3fd319c26734ad6ed91088698ea4328d
hafizsohaibali <hafizsohaibali@gmail.com>
2015-05-21 22:55:28 +0500

    5000 single

--- commandSingle.sh
+++ commandSingle.sh
@@ -1,5 +1,5 @@
-echo '1000 start' >> /data/time.txt
+echo '5000 start' >> /data/time.txt
 date +%s >> /data/time.txt
 python ByteCountSingleMachine.py
 date +%s >> /data/time.txt
-echo '1000 finish' >> /data/time.txt
+echo '5000 finish' >> /data/time.txt
