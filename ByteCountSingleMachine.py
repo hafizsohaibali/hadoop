a782870d29517d4655233714a56405eabcfffe00
hafizsohaibali <hafizsohaibali@gmail.com>
2015-05-21 22:00:55 +0500

    Single Machine code

---
+++ ByteCountSingleMachine.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+tr = open("trainLabels.csv", 'r')  # open training labels' file taht has the ids
+tr.readline()  # headers
+NO_OF_FILES = 1000
+index = 1
+words_file = defaultdict(lambda: 0)
+for line in tr:  # for each line in the file
+    splits = line.split(",")
+    trid = splits[0]  # e.g. a key
+    fop = open("train\\"+trid.replace('"', '') + '.bytes', 'r')  # open the bytes file
+    for byteLine in fop:
+        split = byteLine.replace("\n", "").split(" ")  # break by space
+        for j in range(0, len(split)):
+            str_to_pass = split[j]
+            words_file[str_to_pass] += 1
+    if NO_OF_FILES == index:
+        break;
+    index += 1
+    print str(index)
