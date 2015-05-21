56b9c253f00023e9f782433aa56469707bd0b7c9
hafizsohaibali <hafizsohaibali@gmail.com>
2015-05-22 00:07:30 +0500

    1000 jehandad

--- command.sh
+++ command.sh
@@ -1,6 +1,6 @@
 hadoop fs -rm -r  /user/input/output
-echo '1000  start' >> /mnt/code/output/time.txt
+echo '1000  start' >> /mnt/code/time.txt
 date +%s >> /mnt/code/output/time.txt
-bin/hadoop jar contrib/streaming/hadoop-*streaming*.jar -file /mnt/code/hadoop/mapper.py -mapper /mnt/code/hadoop/mapper.py -file /mnt/code/hadoop/reducer.py -reducer /mnt/code/hadoop/reducer.py -input /user/input/airport/airport-output.csv -output /user/input/output
+bin/hadoop jar contrib/streaming/hadoop-*streaming*.jar -file /mnt/code/hadoop/mapper.py -mapper /mnt/code/hadoop/mapper.py -file /mnt/code/hadoop/reducer.py -reducer /mnt/code/hadoop/reducer.py -input /user/input/train/input.csv -output /user/input/output
 date +%s >> /mnt/code/output/time.txt
-echo '1000 finish' >> /mnt/code/output/time.txt
+echo '1000 finish' >> /mnt/code/time.txt
