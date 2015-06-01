c9f5ff493c49525a3b9eed922f8143bae9f9fb0a
JehandadK <s.jehandad.kamal@gmail.com>
2015-06-01 21:17:16 +0500

    fixed command file

--- command.sh
+++ command.sh
@@ -1,6 +1,10 @@
 hadoop fs -rm -r  /user/input/output
+if [ $? -ne 0 ]; then
+	echo "didnt delete op file"
+    continue
+fi
 echo '1000  start' >> /mnt/code/time.txt
 date +%s >> /mnt/code/output/time.txt
-bin/hadoop jar contrib/streaming/hadoop-*streaming*.jar -file /mnt/code/hadoop/mapper.py -mapper /mnt/code/hadoop/mapper.py -file /mnt/code/hadoop/reducer.py -reducer /mnt/code/hadoop/reducer.py -input /user/input/train/input.csv -output /user/input/output
+hadoop jar /home/hadoop/contrib/streaming/hadoop-streaming.jar -file /mnt/code/hadoop/mapper.py -mapper /mnt/code/hadoop/mapper.py -file /mnt/code/hadoop/reducer.py -combiner /mnt/code/hadoop/reducer.py -reducer /mnt/code/hadoop/reducer.py -input /user/input/train/bytes/* -output /user/input/output
 date +%s >> /mnt/code/output/time.txt
 echo '1000 finish' >> /mnt/code/time.txt
